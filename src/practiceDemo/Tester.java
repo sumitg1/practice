4ae622c59184b0714ab2ec24833bd023dd133cff
riyaanghosh <riyaanghosh@192.168.1.28>
2021-11-28 20:59:37 +0530

    updated code

--- src/practiceDemo/Tester.java
+++ src/practiceDemo/Tester.java
@@ -1,14 +1,5 @@
 package practiceDemo;
 
 public class Tester {
-	
-	public static void main(String arg[]) {
-	int a=0;
-	int b=2;
 
-	int c=a+b;
-	
-    System.out.println(c);
-    //adding
-    }
 }
